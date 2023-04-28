04b78986b6bd18c26d413f5004e56fa807d7472c
harshithbt <35168781+harshithbt@users.noreply.github.com>
2023-04-28 10:45:39 +0530

    Update firebase-config.js

--- src/js/icUtils/firebase-config.js
+++ src/js/icUtils/firebase-config.js
@@ -2,14 +2,14 @@ define(
 	[],
 	() => {
 		const firebaseConfig = {
-            apiKey: "AIzaSyCR_UfcFNlRHgtjyBX-Wn3HQFkfFUs9KrU",
-            authDomain: "influx-capital.firebaseapp.com",
-            databaseURL: "https://influx-capital-default-rtdb.firebaseio.com",
+            apiKey: ".................",
+            authDomain: ".................",
+            databaseURL: ".................",
             projectId: "influx-capital",
-            storageBucket: "influx-capital.appspot.com",
-            messagingSenderId: "609489391928",
-            appId: "1:609489391928:web:689ca8fde590678ac2b1cd",
-            measurementId: "G-6S97YHQEXC"
+            storageBucket: ".................",
+            messagingSenderId: ".................",
+            appId: ".................",
+            measurementId: "................."
           };
 
 
